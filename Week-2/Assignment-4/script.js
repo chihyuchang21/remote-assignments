0115d59d0d55a4926b6e84d6f5f5df89f343b3d2
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-01-26 12:32:42 +0800

    feat: add function(Click Welcome Message to Change Text Problem: Forget to add the function

--- Week-2/Assignment-4/script.js
+++ Week-2/Assignment-4/script.js
@@ -1,4 +1,13 @@
+function changeWelcomeMessage() {
+    var welcomeMessage = document.querySelector('.content-first-title');
+
+    // Change the text to "Have a Good Time!"
+    welcomeMessage.innerText = 'Have a Good Time!';
+}
+
 function showHiddenBoxes() {
     var hiddenBoxes = document.getElementById('hiddenBoxes');
-    hiddenBoxes.classList.remove('hidden');
+
+    // Toggle the 'hidden' class to show/hide the boxes
+    hiddenBoxes.classList.toggle('hidden');
 }

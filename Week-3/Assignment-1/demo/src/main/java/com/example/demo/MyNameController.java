cb51c99e05bcdfdb54f2fb116682c2adb4cd069f
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-01-30 13:43:45 +0800

    refactor/review: W3A4(finished) Just refactor & review!

--- Week-3/Assignment-1/demo/src/main/java/com/example/demo/MyNameController.java
+++ Week-3/Assignment-1/demo/src/main/java/com/example/demo/MyNameController.java
@@ -3,15 +3,18 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyNameController {
 
-    @GetMapping("/myName")
+@RequestMapping(value = "/myName", method = {RequestMethod.GET, RequestMethod.POST})
     public String myName(@CookieValue(value = "userName", defaultValue = "") String userName, Model model) {
         model.addAttribute("userName", userName);
+        //If userName != empty means Cookie exist
         model.addAttribute("hasUserNameCookie", !userName.isEmpty());
-        return "myName";
+        //為了便利標示出.html副檔名
+        return "myName.html";
     }
 }
--- Week-3/Assignment-1/demo/src/main/java/com/example/demo/TrackNameController.java
+++ Week-3/Assignment-1/demo/src/main/java/com/example/demo/TrackNameController.java
@@ -3,17 +3,18 @@ package com.example.demo;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TrackNameController {
 
-    @PostMapping("/trackName")
-    public String trackName(@RequestParam String name, HttpServletResponse response) {
+@RequestMapping(value = "/trackName", method = {RequestMethod.GET, RequestMethod.POST})
+
+public String trackName(@RequestParam String name, HttpServletResponse response) {
         // Set the user's name in cookies
         Cookie cookie = new Cookie("userName", name);
-        cookie.setMaxAge(7 * 24 * 60 * 60); // Cookie expiration time in seconds (e.g., 7 days)
         response.addCookie(cookie);
 
         // Redirect to /myName

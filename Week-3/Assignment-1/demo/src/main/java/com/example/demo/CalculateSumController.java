a020294a0894d92643b46622c7d79c1608048e40
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-01-30 11:38:41 +0800

    Commit: Don't know why I need to commit this file again?

--- Week-3/Assignment-1/demo/src/main/java/com/example/demo/CalculateSumController.java
+++ Week-3/Assignment-1/demo/src/main/java/com/example/demo/CalculateSumController.java
@@ -1,1 +1,55 @@
-S
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static java.lang.Integer.parseInt;
+
+@Controller
+public class CalculateSumController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/data")
+    //ResponseBody: return value(Without it, thymeleaf will return html file)
+    @ResponseBody
+    public String showData(@RequestParam(required = false) String number) {
+        try {
+            //First: Check if the param is null
+            if (number == null){
+                return "Lack of Parameter";
+            }
+
+            //Then: parse String to Int
+            int intValue = parseInt(number);
+
+            if (intValue <= 0) {
+                // Case: Non-positive integer or no integer (0)
+                return "Lack of Parameter";
+            } else {
+                // Case: Valid positive integer
+                int sumResult = calculateSum(intValue);
+                return String.valueOf(sumResult);
+            }
+        } catch (NumberFormatException e) {
+            // Case: Wrong Parameter (non-integer value)
+            return "Wrong Parameter";
+        }
+    }
+
+    @GetMapping("/sum.html")
+    public String sum(){
+        return "sum.html";
+    }
+
+
+    private int calculateSum(int n) {
+        return n * (n + 1) / 2;
+    }
+}
+

8e175c5df66b3bde000ab7484f7038dac9f2b9a6
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-02-05 11:34:38 +0800

    feat/fix: updated W4A2 (finished)

--- Week-4/Assignment-2/script.js
+++ Week-4/Assignment-2/script.js
@@ -3,12 +3,11 @@ The callback function is responsible for handling the data of the AJAX response.
 In this way, you can pass the data to externally defined logic when the response is received.
 */
 
-
 function ajax(src, callback) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function(){
       if (xhr.readyState === 4 && xhr.status === 200){
-          callback(xhr.responseText);
+          callback(JSON.parse(xhr.responseText));
       }
     };
     xhr.open('GET', src, true);
@@ -16,9 +15,14 @@ function ajax(src, callback) {
 }
 
 function render(data) {
-// your code here
-// document.createElement() and appendChild() methods are preferred.
-}
+    const selectDiv = document.getElementById('result');
+
+    data.forEach(product => {
+        const productsDiv = document.createElement('div');
+        productsDiv.textContent = `Product: ${product.name}, Price: ${product.price}, Description: ${product.description}`;
+        selectDiv.appendChild(productsDiv);
+    });
+} // document.createElement() and appendChild() methods are preferred.
 
 ajax(
     'https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products',
